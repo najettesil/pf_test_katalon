eb9d743b7aa083718dd65d4158dffd35e09d256f
Lisette <liisettejan@gmail.com>
2021-09-09 12:00:03 +0800

    Added Test Suite for CreateAcc

---
+++ Test Suites/TS_UserAcc_01/TS_01_CreateAcc.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_01_CreateAcc</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>e508b408-694e-4189-9b00-c39fffca59ec</testSuiteGuid>
+   <testCaseLink>
+      <guid>7745e4f4-df5d-4c98-8ee9-d69850d7ccaf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/MOONBAG FORM/TC_CREATACC_003</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
