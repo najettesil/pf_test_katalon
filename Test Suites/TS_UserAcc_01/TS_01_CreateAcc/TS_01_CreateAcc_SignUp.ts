c61491e482462ebe52d282096dd18c0b82f06976
Lisette Jan <liisettejan@gmail.com>
2021-09-14 19:04:04 +0800

    Run TSC_CreateAcc generate Reports

--- Test Suites/TS_UserAcc_01/TS_01_CreateAcc/TS_01_CreateAcc_SignUp.ts
+++ Test Suites/TS_UserAcc_01/TS_01_CreateAcc/TS_01_CreateAcc_SignUp.ts
@@ -12,79 +12,79 @@
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>d61f80c3-c8c4-40f1-bc34-230e70784a5f</testSuiteGuid>
    <testCaseLink>
-      <guid>8a375a99-c07f-41b9-96fc-7ceac39d3dc1</guid>
+      <guid>c3f5e0a5-ddca-4c2b-8233-1c1eb1bc098e</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_001</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>0eda9a55-245d-4824-8dba-95f1262a2e7e</guid>
+      <guid>8f5787ee-685e-4823-ba19-c6c6a11e3701</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_004</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>7e20ebc0-5490-438f-9eb9-020faf40cdfe</guid>
+      <guid>9397d80d-78c7-4a35-b61a-8801d6f35bf0</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_007</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>025bbf90-2e8e-4387-86ab-491295b16420</guid>
+      <guid>c59af66e-dbb4-4602-82ed-028a4eab9779</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_013</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>78dab829-abcf-4280-a352-81cf4edb422e</guid>
+      <guid>b8ca6beb-1689-4e8e-9285-4b43c0cb8052</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_016</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>5bcd1acc-d146-43b8-9309-2c854300b038</guid>
+      <guid>61f4e8bf-45a1-4afe-a50a-af521afff42f</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_019</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>166dc4c7-e8fd-4ce0-943a-b28aa6da14ac</guid>
+      <guid>5af22fe0-6ca4-4c4d-8a59-ad55b6c7e484</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_022</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>833b9f2c-4ce4-46d5-a758-765c9c1cd33c</guid>
+      <guid>73a9d7f7-f21e-4889-b5c1-3445621a6072</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_023</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>925ac748-3c30-48ce-980b-455a27bf0f5e</guid>
+      <guid>331fd165-6875-46e9-b04c-237cb9b4225b</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_024</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>f8885b02-404e-45bf-abd1-f479d8d1a074</guid>
+      <guid>1c8b81e8-26ac-4290-a579-6b1b3cef0cd9</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_027</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>f0cd9e75-30fa-4fcc-999c-39f45ad1d5c6</guid>
+      <guid>66a414b0-9e05-4d0e-8b04-959f6e1a34a3</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_030</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>6f32ce79-20eb-418e-8e34-e788f89e4c1c</guid>
+      <guid>730a846c-d54f-4550-b88b-2e5bbcda7e16</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_033</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>c0fd78f3-484b-453f-8cde-bd5a2b02a987</guid>
+      <guid>dce8a939-fdf6-46a4-83a0-f761d1df3063</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_036</testCaseId>
@@ -126,13 +126,13 @@
       </variableLink>
    </testCaseLink>
    <testCaseLink>
-      <guid>252a21d6-bc55-464d-98ca-f5722c730847</guid>
+      <guid>da390244-3b5d-4871-a3b7-a0252a706d1a</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_039</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>9ee91a91-128e-4984-9be4-33d8d6bf4969</guid>
+      <guid>3362b946-4e2f-4e28-9036-a064911d41a6</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_040</testCaseId>
@@ -168,253 +168,49 @@
       </variableLink>
    </testCaseLink>
    <testCaseLink>
-      <guid>1e453532-faa7-410f-9979-84843c4ac01a</guid>
+      <guid>eeff5c9f-347f-4416-aaf3-e6090ed0fe52</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_041</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>a69175fa-cecd-4c3e-92e5-8568f1e9c6d2</guid>
+      <guid>e218e082-19b4-4e19-b19d-5ee2e9d1d5f6</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_042</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>17f13fa3-3894-453d-9648-f6e2e9256055</guid>
+      <guid>80ade3f7-0231-46f3-a9ce-a71ad6520d25</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_043</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>3998561e-aef4-40ef-9632-e8f4dcfe1c7e</guid>
+      <guid>1d7cd1c2-1e17-4bf2-8b70-e196456b68c7</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_046</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>d628b476-93ed-4381-9b46-76de283893f8</guid>
+      <guid>99d2dc58-4597-4c10-b55f-aa05e6a2b638</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_049</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>dc92c181-89a1-419a-b75b-27c9ec060f51</guid>
+      <guid>c70ee112-8318-4997-b77c-e9f675a447a4</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_050</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>977c4934-aee6-4dd3-a81e-e11146eec717</guid>
+      <guid>20190d20-ac1c-490a-b2ab-6ea386d481bd</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_051</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>e2d1d7f3-04a0-487e-9843-e83825d9ee68</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_052</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>3fedbc58-2ebc-4547-bca5-13523b9acd3d</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_001</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>9c4df7f1-3011-4c38-ae7a-318204059593</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_004</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>d4f05d73-b2f3-45d3-98c2-736e2cc9fc4f</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_007</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>8b1a2ad9-b96e-4fef-a38e-ed5986b24230</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_013</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>dd486658-73b2-4dc6-bb71-84bfb20b43bc</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_016</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>7f5373c2-c277-4b94-be7a-55dd81c50f14</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_019</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>ae761808-7105-405a-aa17-5b1d490fc80a</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_022</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>b9bf89c9-10c3-48fb-9418-446fb0667c4f</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_023</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>90ca74e0-9b41-434a-909b-6cc1846a0d85</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_024</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>ca0439c8-9bec-4e02-abe5-055e2de9ab54</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_027</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>b44fc8fd-49ab-4b0c-bd7b-c50e823170cf</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_030</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>983dc33d-647d-47f2-819d-10b60bb5a241</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_033</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>91a93222-4edb-4aca-add3-e620391889a2</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_036</testCaseId>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>332e819f-8aad-4f48-975a-5027d318ea95</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>df3db468-69a7-48b9-818f-02436de1ff8e</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>4bd86337-2912-49f0-8951-6f44cf30a488</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>e50c10da-0c02-4e84-9af9-950bb2471d1f</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>bae9b968-70c2-4cf2-9093-1c404f71f777</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>830f540d-26a5-4909-aa79-f5113da36759</variableId>
-      </variableLink>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>71f83ac5-d0d4-4c10-97be-4f0aa5bfafde</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_039</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>c3a900ba-4760-4f96-97bd-db5ad2532879</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_040</testCaseId>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>f812935b-74f2-4d8d-b4d6-5bd99579ec7b</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>e26d0b63-9857-40c9-90de-a5be842fac6c</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>f35f0fe6-ff9a-4dcc-afdd-c929971f2734</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>cb8ab440-0943-4ad6-b3b7-028cbe3759dd</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>3b63d8f7-bf08-4136-838f-1a9768a8c288</variableId>
-      </variableLink>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>12d5dc1d-f2ed-4dc9-8e3e-3bd7517162c3</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_041</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>b984e87d-116f-4779-be3d-b5c3bb6d6001</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_042</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>d45af18b-84a3-4f81-8b7d-e93925369723</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_043</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>0c1c9843-8a73-4fbb-8d1d-65fb9c0369d3</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_046</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>11859c3b-2d2b-4ff0-a8d0-dbfbe8ab9d2d</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_049</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>0e5bd2fe-1bdb-439e-8374-fdb412489654</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_050</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>d1da47de-eaf2-4b1e-b929-e6b6c2d0aef6</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_051</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>4352b364-7db6-4430-95cd-2824eb325fd8</guid>
+      <guid>e4315690-a865-4187-95c4-49b0cd7a9eee</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/TC_UserAcc_01/TC_01_CreateAcc/SIGN UP PAGE/TC_CREATEACC_052</testCaseId>
